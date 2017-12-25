b1669561d25008d8637e09ae88ab8f5aee02ac51
Rémi Cohen-Scali <remi@jayacode.fr>
2017-12-25 18:17:32 +0100

    Added common type definitions in a common header

--- clang-tidy/pagesjaunes/ExecSQLCommon.h
+++ clang-tidy/pagesjaunes/ExecSQLCommon.h
@@ -19,10 +19,21 @@
 #define GENERATION_SOURCE_FILENAME_EXTENSION ".pc"
 #define GENERATION_HEADER_FILENAME_EXTENSION ".h"
 
+// Regex values
+#define PAGESJAUNES_REGEX_EXEC_SQL_FETCH_REQ_RE \
+  "EXEC[[:space:]]+SQL[[:space:]]+(fetch|FETCH)[[:space:]]*(:?[[:space:]]*[A-Za-z0-9]+)[[:space:]]+(INTO|into)?[[:space:]]*(.*)[[:space:]]*;"
+#define PAGESJAUNES_REGEX_EXEC_SQL_FETCH_FILELINE \
+  "^(.*)#([0-9]+)$"
+#define PAGESJAUNES_REGEX_EXEC_SQL_FETCH_TMPL_REPEAT_RE \
+  "@repeat on ([[:word:]]+)\\{[[:space::]]*([[:word:]]+)[[:space::]]*(,[[:space::]]*([[:word:]]+))\\}"
+#define PAGESJAUNES_REGEX_EXEC_SQL_FETCH_LINE_DEFINE_RE \
+  "^#line ([0-9]+) \"(.*)\"$"
+
 // Common types
 using namespace clang;
 using namespace clang::ast_matchers;
 using string2_map = std::map<std::string, std::string>;
+using ushort_string_map = std::map<unsigned short, std::map<std::string, std::string>>;  
 using map_vector_string = std::map<std::string, std::vector<std::string>>;
 using map_replacement_values = std::map<std::string, std::string>;
 using map_comment_map_replacement_values = std::map<std::string, std::map<std::string, std::string>>;
